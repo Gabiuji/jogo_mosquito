4ba42a33e234a54a97a89d904e7f6bf84d49fc5d
Gabiuji <ogabrieliuji@gmail.com>
2025-04-07 15:54:20 -0400

    randomizando posições

--- jogo.js
+++ jogo.js
@@ -6,5 +6,23 @@ function ajustarTamanho() {
   largura = window.innerWidth;
   altura = window.innerHeight;
 }
-
 ajustarTamanho();
+
+function posicaoRandomica() {
+    var posicaoX = Math.floor(Math.random() * (largura - 50));
+    var posicaoY = Math.floor(Math.random() * (altura - 50));
+
+    posixaoX = posicaoX < 0 ? 0 : posicaoX; // Garantindo que a posição X não seja negativa
+    posicaoY = posicaoY < 0 ? 0 : posicaoY; // Garantindo que a posição Y não seja negativa
+
+    //criando o elemento html
+
+    var mosquito = document.createElement("img");
+    mosquito.src = "imagens/mosca.png";
+    mosquito.className = "mosquito1";
+    mosquito.style.left = posicaoX + "px";
+    mosquito.style.top = posicaoY + "px";
+    mosquito.style.position = "absolute";
+    document.body.appendChild(mosquito);
+}
+
