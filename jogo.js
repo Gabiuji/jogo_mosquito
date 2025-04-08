f462e3a77580e965b341360bb621a9782ba59f29
Gabiuji <ogabrieliuji@gmail.com>
2025-04-08 12:29:25 -0400

    tamanho e lado randomico

--- jogo.js
+++ jogo.js
@@ -19,10 +19,41 @@ function posicaoRandomica() {
 
     var mosquito = document.createElement("img");
     mosquito.src = "imagens/mosca.png";
-    mosquito.className = "mosquito1";
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio(); // Chama a função para definir a classe aleatória
     mosquito.style.left = posicaoX + "px";
     mosquito.style.top = posicaoY + "px";
     mosquito.style.position = "absolute";
     document.body.appendChild(mosquito);
+
 }
 
+function tamanhoAleatorio(){
+
+  var classes = Math.floor(Math.random() * 3);
+
+  switch(classes){
+    case 0:
+        return "mosquito1";
+        
+    case 1:
+        return "mosquito2";
+        
+    case 2:
+        return "mosquito3";
+        
+  }
+
+}
+
+function ladoAleatorio(){
+  var classes = Math.floor(Math.random() * 2);
+
+  switch(classes){
+    case 0:
+        return "ladoA";
+        
+    case 1:
+        return "ladoB";
+        
+  }
+}
